a6e4fa5d43dfcb9b655cf462c9aebd64d050a1d6
cristobal-2207 <cristobalgutierreznavarro@gmail.com>
2020-11-18 00:25:05 -0300

    Se avanza en desafío, se crean vistas, modelos y controladores.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "wines#new"
+  resources :wines
+  resources :assemblies
+  resources :strains
+  resources :enologists
+  resources :magazines
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
